fd3b4d849e94aaff32a242107f420bb72c50b9a7
Jordan Castro <jordan@grupojvm.com>
2023-08-26 11:07:09 -0500

    Clean code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,4 @@
-// use server::server::run_server;
-// use client::client::ClientCalls;
-use std::process::Command;
-
-// mod client;
-// mod server;
-// mod ppc;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    // let _ = run_server().await;
-    // // Run server and client in seperate thread
-    // let (tx, mut rx) = mpsc::channel(32);
-    // tokio::spawn(async move {
-    //     // Initialize the client, make gRPC calls, and send responses through the channel
-    //     let mut client_calls = ClientCalls::new().await.expect("ClientCalls returned.");
-    //     // Make requests
-    //     let res = client_calls.query_call("user".to_string()).await.expect("Returne QueryResponse");
-    //     tx.send(res).await.expect("Send error");
-    // });
-    
-    // // Simulate receiving!
-    // while let Some(response) = rx.recv().await {
-    //     println!("Received response: {:?}", response);
-    // }
-
     Ok(())
 }
--- src/ppc.rs
+++
@@ -1,3 +0,0 @@
-pub mod ppc {
-    tonic::include_proto!("myapp"); // The string specified here must match the proto package name
-}
